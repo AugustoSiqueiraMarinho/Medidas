3bf5dbde67ab86b5f88f7ae99552fe511c9a0d8e
Augusto S <augustosiqueira000@gmail.com>
2025-09-26 23:36:18 -0300

    Exercíício Finalizado

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// conversor de medidas
+double m , cm, km;
+
+Console.Write("Entre com a medida (em metros): ");
+m = Convert.ToDouble(Console.ReadLine());
+
+cm = m * 100;
+km = m / 1000; 
+
+Console.WriteLine("-- Equevalência ---");
+Console.WriteLine($"{cm} cm");
+Console.WriteLine($"{m} m");
+Console.WriteLine($"{km} Km");
+
